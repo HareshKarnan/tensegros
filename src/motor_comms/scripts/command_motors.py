581cb384ae0399b7eab788d160e427df8116a730
Haresh Miriyala <haresh.miriyala@gmail.com>
2018-04-10 12:32:51 -0500

    updated

--- src/motor_comms/scripts/command_motors.py
+++ src/motor_comms/scripts/command_motors.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
-from std_msgs.msg import Int32MultiArray
+from std_msgs.msg import Float32MultiArray
 from serial import *
+from math import *
 ser = Serial(port='/dev/ttyACM0',baudrate=115200,bytesize=EIGHTBITS,parity=PARITY_NONE,stopbits=STOPBITS_ONE)
 
 def callback(data):
     # do stuff with the data
-    rospy.loginfo('motor1 data received : ' + str(data.data[0]))
-    rospy.loginfo('motor2 data received : ' + str(data.data[1]))
-    rospy.loginfo('motor3 data received : ' + str(data.data[2]))
-    rospy.loginfo('motor4 data received : ' + str(data.data[3]))
-    rospy.loginfo('motor5 data received : ' + str(data.data[4]))
-    rospy.loginfo('motor6 data received : ' + str(data.data[5]))
+    # ang2inch =  360.0/(pi*0.395**2)
+    ang2inch = 1
+    rospy.loginfo('motor1 data received : ' + str(ang2inch*data.data[0]))
+    rospy.loginfo('motor2 data received : ' + str(ang2inch*data.data[1]))
+    rospy.loginfo('motor3 data received : ' + str(ang2inch*data.data[2]))
+    rospy.loginfo('motor4 data received : ' + str(ang2inch*data.data[3]))
+    rospy.loginfo('motor5 data received : ' + str(ang2inch*data.data[4]))
+    rospy.loginfo('motor6 data received : ' + str(ang2inch*data.data[5]))
     while (ser.isOpen()):
-        string = '1:' + str(data.data[0]) + ';'
+        string = '5:' + str(ang2inch*(-data.data[0])) + ';'
         ser.write(string)
-        string = '2:' + str(-data.data[1]) + ';'
+        string = '1:' + str(ang2inch*(data.data[1])) + ';'
         ser.write(string)
-        string = '3:' + str(data.data[2]) + ';'
+        string = '6:' + str(ang2inch*data.data[2]) + ';'
         ser.write(string)
-        string = '4:' + str(-data.data[3]) + ';'
+        string = '2:' + str(ang2inch*(-data.data[3])) + ';'
         ser.write(string)
-        string = '5:' + str(-data.data[4]) + ';'
+        string = '3:' + str(ang2inch*(data.data[4])) + ';'
         ser.write(string)
-        string = '6:' + str(data.data[5]) + ';'
+        string = '4:' + str(ang2inch*(-data.data[5])) + ';'
         ser.write(string)
         break
 
@@ -32,7 +35,7 @@ def callback(data):
 
 def listener():
     rospy.init_node('motors_listener',anonymous = False)
-    rospy.Subscriber('motors_command',Int32MultiArray,callback)
+    rospy.Subscriber('motors_command',Float32MultiArray,callback)
     # keep python from exiting
     rospy.spin()
 
--- src/motor_comms_test/scripts/motor_comms_test.py
+++ src/motor_comms_test/scripts/motor_comms_test.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import Int32MultiArray
+from std_msgs.msg import Float32MultiArray
 
 def talker():
     # topic name : motors_command
-    pub = rospy.Publisher('motors_command', Int32MultiArray, queue_size=6)
+    pub = rospy.Publisher('motors_command', Float32MultiArray, queue_size=6)
     # name of the node : motor_comms_test
     rospy.init_node('motor_comms_test',anonymous = False)
     while not rospy.is_shutdown():
         # get input from user for the motors and publish it
-        a = map(int,raw_input().split())
+        a = map(float,raw_input().split())
         if len(a)==6:
             pass
         else:
             a = [0,0,0,0,0,0]
 
         rospy.loginfo('Publishing motor test data : ' + str(a))
-        pub.publish(Int32MultiArray(data=a))
+        pub.publish(Float32MultiArray(data=a))
 
 
 if __name__ == '__main__':
